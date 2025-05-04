c5ac91d44c4fecef0e10529c898f238817538506
jerico-c <jericochristianto@gmail.com>
2025-05-04 10:38:20 +0700

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
+import App from './app.js';
+ 
 document.addEventListener('DOMContentLoaded', async () => {
-  console.log('Hello, world!');
-});
-
-window.addEventListener('hashchange', async () => {
-  console.log('Hash changed!');
+  const content = document.querySelector('#content');
+  const app = new App({ content });
+  await app.renderPage();
+ 
+  window.addEventListener('hashchange', async () => {
+    await app.renderPage();
+  });
 });
